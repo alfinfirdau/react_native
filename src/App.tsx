3207bea7b01fcbca2c3973e8084235978de72d94
alfinfirdau <pelukissalam@gmail.com>
2024-05-04 23:50:36 +0700

    firts commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import beranda from './screens/beranda';
+import notifikasi from './screens/notifikasi';
+import setelan from './screens/setelan';
+
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Beranda" component={beranda} options={{
+          tabBarLabel: 'Beranda',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}/>
+      <Tab.Screen name="Notifikasi" component={notifikasi} options={{
+          tabBarLabel: 'Notifikasi',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          ),
+        }}/>
+      <Tab.Screen name="Setelan" component={setelan} options={{
+          tabBarLabel: 'Setelan',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="cog" color={color} size={26} />
+          ),
+        }}/>
+    </Tab.Navigator>
+  );
+}
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MyTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ src/screens/setelan.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const setelan = () => {
+  return (
+    <View>
+    </View>
+  )
+}
+
+export default setelan
+
+const styles = StyleSheet.create({})
